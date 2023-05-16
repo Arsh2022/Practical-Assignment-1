a855ab77b2149b257fd73cce87c7e3914682b0d7
Arsh2022 <110711867+Arsh2022@users.noreply.github.com>
2023-05-16 12:49:43 +1000

    Create IBDMS.java

---
+++ IBDMS.java
@@ -0,0 +1,15 @@
+package ibdms;
+
+/**
+ *
+ * @author Arsh Kataria
+ */
+public class IBDMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+    
+}
